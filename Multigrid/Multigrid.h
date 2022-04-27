bf1292124def62c982274def1810dfeffe55114b
luxi <448153639@qq.com>
2022-04-27 17:45:33 +0800

    h->n

--- Multigrid/Multigrid.h
+++ Multigrid/Multigrid.h
@@ -17,7 +17,6 @@ public:
     Eigen::MatrixXd      data;
     GridBdryType         bdryType;
     double               grid_length;     // h
-    double               inv_grid_length; // floor(?) = n
     unsigned             grid_size;     // n-1
     int                  grid_node_num;
     int                  max_itr_num; // maximum times for iteration
@@ -39,23 +38,14 @@ template <int dim>
 Multigrid<dim>::Multigrid(const Json::Value &root)
 {
     // 写入属性
-    try
-    {
-        grid_length = root["grid_h"].asDouble();
-        if (grid_length <= 0)
-            throw 1;
-        inv_grid_length = 1.0 / grid_length;
-        grid_size = floor(inv_grid_length) - 1; // n - 1
-        grid_length = 1.0 / (grid_size - 1);    // h 重新计算，保证等分
-        grid_node_num = grid_size; //TODO
-        if (dim == 2)
-            grid_node_num *= grid_size;
-    }
-    catch (...)
-    {
-        std::cerr << "Invalid parameter grid_h." << std::endl;
-        exit(1);
-    }
+    grid_size = root["grid_n"].asInt() - 1; // n - 1
+    if(grid_size <= 0 && (grid_size & (grid_size + 1)) != 0)
+        throw std::exception("Invalid grid_n");
+    grid_length = 1.0 / (grid_size + 1);
+    
+    grid_node_num = grid_size;
+    if (dim == 2)
+        grid_node_num *= grid_size;
 
     // 生成网格
     if(dim == 1)
